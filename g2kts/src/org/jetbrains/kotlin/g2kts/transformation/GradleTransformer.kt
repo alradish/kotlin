9d58ce59cce89c04d6aa3a9c43996992454f10fb
Alexander Raish <leidenn2509@gmail.com>
2019-12-08 22:20:19 +0700

    project property transformer

--- g2kts/src/org/jetbrains/kotlin/g2kts/transformation/GradleTransformer.kt
+++ g2kts/src/org/jetbrains/kotlin/g2kts/transformation/GradleTransformer.kt
@@ -8,6 +8,7 @@ package org.jetbrains.kotlin.g2kts.transformation
 import org.jetbrains.kotlin.g2kts.GNode
 import org.jetbrains.kotlin.g2kts.transformation.groovy2kts.BuildScriptBlockTransformation
 import org.jetbrains.kotlin.g2kts.transformation.groovy2kts.ConfigurationBlockTransformation
+import org.jetbrains.kotlin.g2kts.transformation.groovy2kts.ProjectPropertyTransformation
 import org.jetbrains.kotlin.g2kts.transformation.groovy2kts.TaskTransformation
 
 object GradleTransformer {
@@ -22,6 +23,7 @@ object GradleTransformer {
             +BuildScriptBlockTransformation()
             +TaskTransformation()
             +ConfigurationBlockTransformation()
+            +ProjectPropertyTransformation()
         }.transformations)
     }
 
